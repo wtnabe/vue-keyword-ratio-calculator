971ed145d673beab1a0f5c8f84c07ff2f4608bab
wtnabe <wtnabe@gmail.com>
2021-06-17 11:53:19 +0900

    add dependency eslint-config-standard

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'standard',
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {},
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
